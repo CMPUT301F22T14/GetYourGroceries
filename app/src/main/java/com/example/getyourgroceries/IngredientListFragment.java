9b67577defca79b63678a444f0baa327b84daf23
Ali1215 <55634235+Ali1215@users.noreply.github.com>
2022-10-23 19:22:50 -0600

    started ingredient storage
    Ingredient Storage List

--- app/src/main/java/com/example/getyourgroceries/IngredientListFragment.java
+++ app/src/main/java/com/example/getyourgroceries/IngredientListFragment.java
@@ -8,6 +8,12 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.getyourgroceries.entity.storedIngredient;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class IngredientListFragment extends Fragment {
 
@@ -19,6 +25,11 @@ public class IngredientListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ingredient_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
+        storedIngredient s = new storedIngredient("Apple", 12, 12.0, "Fruit", new Date(2012,12,12), "Pantry");
+        getParentFragmentManager().beginTransaction().add(R.id.linearLayoutIngredients, IngredientFragment.newInstance(s)).commit();
+        getParentFragmentManager().beginTransaction().add(R.id.linearLayoutIngredients, IngredientFragment.newInstance(s)).commit();
+        getParentFragmentManager().beginTransaction().add(R.id.linearLayoutIngredients, IngredientFragment.newInstance(s)).commit();
+        return v;
     }
 }

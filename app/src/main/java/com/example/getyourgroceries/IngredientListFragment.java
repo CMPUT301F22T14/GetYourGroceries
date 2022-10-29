d4a6d4842060cc0a4497fe9755be57804f8805ec
Ryder Catonio <rydercatonio@Ryders-MacBook-Air.local>
2022-10-28 20:01:21 -0600

    Testv2

--- app/src/main/java/com/example/getyourgroceries/IngredientListFragment.java
+++ app/src/main/java/com/example/getyourgroceries/IngredientListFragment.java
@@ -59,7 +59,6 @@ public class IngredientListFragment extends Fragment {
         sortDropDown = v.findViewById(R.id.sortIngredientSpinner);
         ArrayAdapter<CharSequence> sortAdapter = ArrayAdapter.createFromResource(context,R.array.ingredientSortBy,android.R.layout.simple_spinner_item);
         sortDropDown.setAdapter(sortAdapter);
-        System.out.println("test");
         return v;
     }
 }

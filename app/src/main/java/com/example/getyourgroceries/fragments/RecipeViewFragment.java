b095d56f6c14891ed9e880d73f7da00df4c3ae3e
Mira <mirapatel2512@gmail.com>
2022-11-03 15:05:01 -0600

    more work on vire recipe fragment

--- app/src/main/java/com/example/getyourgroceries/fragments/RecipeViewFragment.java
+++ app/src/main/java/com/example/getyourgroceries/fragments/RecipeViewFragment.java
@@ -1,17 +1,38 @@
 package com.example.getyourgroceries.fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.getyourgroceries.R;
 import com.example.getyourgroceries.entity.Recipe;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class RecipeViewFragment extends Fragment {
@@ -20,18 +41,51 @@ public class RecipeViewFragment extends Fragment {
     private Recipe viewRecipe;
     FirebaseFirestore db;
 
-    public RecipeViewFragment() { super();}
+    public RecipeViewFragment() {
+        super();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_view_recipe, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_recipe, container, false);
         Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setHasOptionsMenu(true);
         return view;
     }
 
+    /**
+     * The onViewCreated method will be called once the view has been created.
+     *
+     * @param view               The created view.
+     * @param savedInstanceState The saved state.
+     */
+    @SuppressLint("SimpleDateFormat")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+        // Initialization.
+        ConstraintLayout viewIngredientLayout = requireActivity().findViewById(R.id.view_recipe_layout);
+        viewIngredientLayout.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO);
+
+        if (getArguments() != null) {
+            viewRecipe = (Recipe) getArguments().getSerializable("viewRecipe");
+        }
+
+
+        //Populate fields if its a view
+        ImageView image = requireActivity().findViewById(R.id.image);
+        TextView title = requireActivity().findViewById(R.id.titleTextField);
+        TextView prepTime = requireActivity().findViewById(R.id.prepTimeTextField);
+        TextView category = requireActivity().findViewById(R.id.categoryTextField);
+        //TextView commentsText = requireActivity().findViewById(R.id.change_recipe_comments);
+
+
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("Recipes");
+
+
+    }
 
 }

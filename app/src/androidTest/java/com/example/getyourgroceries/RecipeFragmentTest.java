31657af542c7c3cf8f70ccf64a87591a154a3b69
kush2021 <kushpadalia2002@gmail.com>
2022-11-04 16:12:35 -0600

    Testing completed.

--- app/src/androidTest/java/com/example/getyourgroceries/RecipeFragmentTest.java
+++ app/src/androidTest/java/com/example/getyourgroceries/RecipeFragmentTest.java
@@ -1,9 +1,11 @@
 package com.example.getyourgroceries;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.getyourgroceries.entity.IngredientStorage;
+import com.example.getyourgroceries.entity.Recipe;
 import com.example.getyourgroceries.entity.RecipeStorage;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.robotium.solo.Solo;
@@ -22,6 +25,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.Objects;
 
 /**
  * All UI tests for recipes
@@ -176,6 +181,92 @@ public class RecipeFragmentTest {
         SwitchCompat switch_sorting = (SwitchCompat)solo.getView(R.id.sortingSwitchRecipe);
         solo.clickOnView(switch_sorting);
         //Need to assert that the first item is the correct sorted item
+    }
+    @Test
+    public void testViewRecipe() {
+        testAddRecipe();
+        solo.assertCurrentActivity("Wrong Activity!", MainActivity.class);
+        BottomNavigationItemView navItem = (BottomNavigationItemView) solo.getView(R.id.recipe_icon);
+        solo.clickOnView(navItem.getChildAt(1));
+        ListView recipes = (ListView) solo.getView(R.id.recipe_list);
+        int size = recipes.getAdapter().getCount();
+        for (int i = 0; i < size; i++) {
+            Recipe recipe = (Recipe) recipes.getAdapter().getItem(i);
+            if (Objects.equals(recipe.getName(), "Test")) {
+                solo.clickInList(i);
+                break;
+            }
+        }
+    }
+    @Test
+    public void testAddRecipe() {
+        solo.assertCurrentActivity("Wrong Activity!", MainActivity.class);
+        BottomNavigationItemView navItem = (BottomNavigationItemView) solo.getView(R.id.recipe_icon);
+        solo.clickOnView(navItem.getChildAt(1));
+        ListView recipes = (ListView) solo.getView(R.id.recipe_list);
+        int size = recipes.getAdapter().getCount();
+        solo.clickOnButton("Add Recipe");
+        solo.enterText((EditText) solo.getView(R.id.change_recipe_name), "Test");
+        assertTrue(solo.waitForText("Test", 1, 2000));
+        solo.enterText((EditText) solo.getView(R.id.change_recipe_prep_time), "20");
+        assertTrue(solo.waitForText("20", 1, 2000));
+        solo.enterText((EditText) solo.getView(R.id.change_recipe_servings), "3");
+        assertTrue(solo.waitForText("3", 1, 2000));
+        solo.pressSpinnerItem(0, 1);
+        assertTrue(solo.waitForText("Baking", 1, 2000));
+        solo.enterText((EditText) solo.getView(R.id.change_recipe_comments), "This is a test.");
+        assertTrue(solo.waitForText("This is a test.", 1, 2000));
+        solo.clickOnButton("Confirm");
+        ListView recipesFinal = (ListView) solo.getView(R.id.recipe_list);
+        assertEquals(recipesFinal.getAdapter().getCount(), size + 1);
+    }
+    @Test
+    public void testEditRecipe() {
+        testAddRecipe();
+        solo.assertCurrentActivity("Wrong Activity!", MainActivity.class);
+        BottomNavigationItemView navItem = (BottomNavigationItemView) solo.getView(R.id.recipe_icon);
+        solo.clickOnView(navItem.getChildAt(1));
+        ListView recipes = (ListView) solo.getView(R.id.recipe_list);
+        int size = recipes.getAdapter().getCount();
+        for (int i = 0; i < size; i++) {
+            Recipe recipe = (Recipe) recipes.getAdapter().getItem(i);
+            if (Objects.equals(recipe.getName(), "Test")) {
+                solo.clickInList(i);
+                solo.clickOnImageButton(0);
+                solo.enterText((EditText) solo.getView(R.id.change_recipe_name), "Test");
+                assertTrue(solo.waitForText("Test", 1, 2000));
+                solo.enterText((EditText) solo.getView(R.id.change_recipe_prep_time), "20");
+                assertTrue(solo.waitForText("20", 1, 2000));
+                solo.enterText((EditText) solo.getView(R.id.change_recipe_servings), "3");
+                assertTrue(solo.waitForText("3", 1, 2000));
+                solo.pressSpinnerItem(0, 2);
+                assertTrue(solo.waitForText("Microwaving", 1, 2000));
+                solo.enterText((EditText) solo.getView(R.id.change_recipe_comments), "This is a test.");
+                assertTrue(solo.waitForText("This is a test.", 1, 2000));
+                solo.clickOnButton("Confirm");
+                break;
+            }
+        }
+    }
+    @Test
+    public void testDeleteRecipe() {
+        testAddRecipe();
+        solo.assertCurrentActivity("Wrong Activity!", MainActivity.class);
+        BottomNavigationItemView navItem = (BottomNavigationItemView) solo.getView(R.id.recipe_icon);
+        solo.clickOnView(navItem.getChildAt(1));
+        ListView recipes = (ListView) solo.getView(R.id.recipe_list);
+        int size = recipes.getAdapter().getCount();
+        for (int i = 0; i < size; i++) {
+            Recipe recipe = (Recipe) recipes.getAdapter().getItem(i);
+            if (Objects.equals(recipe.getName(), "Test")) {
+                solo.clickLongInList(i);
+                solo.clickOnButton("Yes");
+                solo.sleep(2000);
+                break;
+            }
+        }
+        ListView recipesFinal = (ListView) solo.getView(R.id.recipe_list);
+        assertEquals(recipesFinal.getAdapter().getCount(), size - 1);
     }
     @After
     public void tearDown() throws Exception {

ab7cba97ff8a8f49516edf8bcf861c839b11ae1d
Ali1215 <55634235+Ali1215@users.noreply.github.com>
2022-11-15 21:21:35 -0700

    View shopping list
    added buttons for delete and collected, but they are inactive

--- app/src/main/java/com/example/getyourgroceries/fragments/ShoppingListFragment.java
+++ app/src/main/java/com/example/getyourgroceries/fragments/ShoppingListFragment.java
@@ -1,16 +1,25 @@
-package com.example.getyourgroceries;
+package com.example.getyourgroceries.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.getyourgroceries.R;
+import com.example.getyourgroceries.adapters.ShoppingListAdapter;
+import com.example.getyourgroceries.entity.Ingredient;
+
+import java.util.ArrayList;
+
 public class ShoppingListFragment extends Fragment {
 
+    ListView shoppingListView;
     public ShoppingListFragment(){
 
     }
@@ -19,6 +28,14 @@ public class ShoppingListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        View view =  inflater.inflate(R.layout.fragment_shopping_list, container, false);
+        shoppingListView = view.findViewById(R.id.shoppingListView);
+        ArrayList<Ingredient> shoppingItems = new ArrayList<>();
+        ArrayAdapter<Ingredient> adapter = new ShoppingListAdapter(requireActivity().getBaseContext(), shoppingItems);
+        shoppingListView.setAdapter(adapter);
+        adapter.add(new Ingredient("test", 12, 13.23, "Fruits"));
+        adapter.notifyDataSetChanged();
+
+        return view;
     }
 }

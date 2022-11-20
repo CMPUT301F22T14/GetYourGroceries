159550ee438a77b47648f63d7f237151c6eaee04
Mira <mirapatel2512@gmail.com>
2022-11-19 21:38:24 -0700

    added animation to splash screen

--- app/src/main/java/com/example/getyourgroceries/SplashActivity.java
+++ app/src/main/java/com/example/getyourgroceries/SplashActivity.java
@@ -3,16 +3,21 @@ package com.example.getyourgroceries;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView logo = findViewById(R.id.appLogo);
 
         Intent iHome = new Intent(SplashActivity.this, MainActivity.class);
 
@@ -20,8 +25,12 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(iHome);
+                finish();
+
             }
         },4000);
 
+
     }
+
 }

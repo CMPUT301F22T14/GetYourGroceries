45fc398123e7b97db8315ab59c6f761c8382d598
Jack Parsons <jackpar1113@gmail.com>
2022-11-26 22:15:21 -0700

    add javadoc and empty constructor to scaled recipe

--- app/src/main/java/com/example/getyourgroceries/entity/ScaledRecipe.java
+++ app/src/main/java/com/example/getyourgroceries/entity/ScaledRecipe.java
@@ -1,27 +1,55 @@
 package com.example.getyourgroceries.entity;
 
+/**
+ * Recipe that gets scaled on meal plans
+ */
 public class ScaledRecipe extends Recipe {
-
     private Recipe recipe;
     private int scale;
 
+    /**
+     * Empty constructor
+     */
+    public ScaledRecipe() {}
+
+    /**
+     * Constructor
+     * @param recipe sets internal recipe object
+     * @param scale number scale of the recipe
+     */
     public ScaledRecipe(Recipe recipe, int scale) {
         this.recipe = recipe;
         this.scale = scale;
     }
 
+    /**
+     * Fetches recipe
+     * @return recipe object
+     */
     public Recipe getRecipe() {
         return recipe;
     }
 
+    /**
+     * Fetches scale of recipe
+     * @return number scale
+     */
     public int getScale() {
         return scale;
     }
 
+    /**
+     * Sets the internal recipe
+     * @param recipe object to set to
+     */
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
 
+    /**
+     * Sets the scale of the recipe
+     * @param scale number to set to
+     */
     public void setScale(int scale) {
         this.scale = scale;
     }

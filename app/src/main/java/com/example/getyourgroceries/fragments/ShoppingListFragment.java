f91ec59cc77ef3c41c3850f922dffc3ba322e6c1
Ali1215 <55634235+Ali1215@users.noreply.github.com>
2022-11-27 13:53:33 -0700

    finish calc

--- app/src/main/java/com/example/getyourgroceries/fragments/ShoppingListFragment.java
+++ app/src/main/java/com/example/getyourgroceries/fragments/ShoppingListFragment.java
@@ -19,10 +19,18 @@ import androidx.annotation.Nullable;
 import com.example.getyourgroceries.R;
 import com.example.getyourgroceries.adapters.ShoppingListAdapter;
 import com.example.getyourgroceries.entity.Ingredient;
+import com.example.getyourgroceries.entity.IngredientStorage;
+import com.example.getyourgroceries.entity.MealPlan;
+import com.example.getyourgroceries.entity.MealPlanDay;
+import com.example.getyourgroceries.entity.MealPlanStorage;
+import com.example.getyourgroceries.entity.Recipe;
 import com.example.getyourgroceries.entity.RecipeStorage;
+import com.example.getyourgroceries.entity.ScaledRecipe;
+import com.example.getyourgroceries.entity.StoredIngredient;
 import com.google.android.material.materialswitch.MaterialSwitch;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ShoppingListFragment extends Fragment {
     ListView shoppingListView;
@@ -39,7 +47,7 @@ public class ShoppingListFragment extends Fragment {
 
         actionBar.setTitle("Shopping List");
         shoppingListView = view.findViewById(R.id.shoppingListView);
-        ArrayList<Ingredient> shoppingItems = new ArrayList<>();
+        ArrayList<Ingredient> shoppingItems = calculateItems();
         ArrayAdapter<Ingredient> adapter = new ShoppingListAdapter(requireActivity().getBaseContext(), shoppingItems);
         shoppingListView.setAdapter(adapter);
 
@@ -72,9 +80,62 @@ public class ShoppingListFragment extends Fragment {
             }
         });
 
-        adapter.add(new Ingredient("test", 12, 13.23, "Fruits"));
         adapter.notifyDataSetChanged();
 
         return view;
+    }
+
+    public ArrayList<Ingredient> calculateItems(){
+        ArrayList<Ingredient> calculatedItems = new ArrayList<>();
+        for (MealPlan mealPlan: MealPlanStorage.getInstance().getMealPlanList()){
+            for (MealPlanDay mealPlanDay: mealPlan.getMealPlanDays()){
+                for (Ingredient ingredient : mealPlanDay.getIngredientList()){
+                    // now loop over calculated items and see if matching ingredient is there
+                    boolean found = false;
+                    for (Ingredient calcItem : calculatedItems){
+                        if (calcItem.getDescription().equals(ingredient.getDescription())){
+                            calcItem.setAmount(calcItem.getAmount() + ingredient.getAmount());
+                            found = true;
+                        }
+                    }
+                    // if not found then add to items
+                    if (!found){
+                        calculatedItems.add(new Ingredient(ingredient.getDescription(), ingredient.getAmount(), ingredient.getUnit(), ingredient.getCategory()));
+                    }
+                }
+                for (ScaledRecipe recipe : mealPlanDay.getRecipeList()){
+                    for (Ingredient ingredient : recipe.getRecipe().getIngredientList()){
+                        boolean found = false;
+                        for (Ingredient calcItem : calculatedItems){
+                            if (calcItem.getDescription().equals(ingredient.getDescription())){
+                                calcItem.setAmount(calcItem.getAmount() + (ingredient.getAmount())*recipe.getScale());
+                                found = true;
+                            }
+                        }
+                        // if not found then add to items
+                        if (!found){
+                            calculatedItems.add(new Ingredient(ingredient.getDescription(), ingredient.getAmount()*recipe.getScale(), ingredient.getUnit(), ingredient.getCategory()));
+                        }
+                    }
+                }
+            }
+        }
+
+
+        Iterator<Ingredient> itr = calculatedItems.iterator();
+        while (itr.hasNext()){
+            Ingredient ingredient = itr.next();
+            for (StoredIngredient storedIngredient: IngredientStorage.getInstance().getIngredientList()){
+                if (ingredient.getDescription().equals(storedIngredient.getDescription())){
+                    int res = ingredient.getAmount() - storedIngredient.getAmount();
+                    if (res < 0)
+                        itr.remove();
+                    else
+                        ingredient.setAmount(res);
+                }
+            }
+        }
+        return calculatedItems;
+
     }
 }
